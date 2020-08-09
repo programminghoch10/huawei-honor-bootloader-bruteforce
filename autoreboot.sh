48c2a10dc0996f0fb3fefe640c82388febaf94ed
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2020-08-09 19:21:12 +0200

    added more info to autoreboot shell script

--- autoreboot.sh
+++ autoreboot.sh
@@ -2,6 +2,9 @@
 # this script can be run in the background when bruteforcing on a linux machine
 # it will automatically reboot adb devices into the bootloader
 # run it in the background like this: ./autoreboot.sh &
+# use this only if your bootloader doesn't have the reboot protection,
+#  or if it has reboot protection and you've set the autoreboot variable MANUALLY to True
+#  else the bruteforce script could stop executing randomly
 while true
 do
 adb wait-for-usb-device
